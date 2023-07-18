fe6aa19ed081325bb9a462aadbbc4430a916a8af
OrhanYigitDurmaz <orhanyigitv2durmaz@gmail.com>
2023-07-18 15:23:50 +0300

    Update SimShiftElite.ino

--- SimShiftElite/SimShiftElite.ino
+++ SimShiftElite/SimShiftElite.ino
@@ -5,28 +5,24 @@
 // 1: Clean up messy code and add a better logic
 // 2: Write the API docs for the serial commands
 // 3: Make the GUI (if someone wants to help, im here: orhanyigitv2durmaz [at] gmail [dot] com ,  github.com/OrhanYigitDurmaz/SimShiftElite-GUI) )
-// 4: 
+// 4: Add Smoothed library for adc readings
 
 #include <Arduino.h>
 #include <EEPROM.h>
 #include <Joystick.h>
 
-const int adcPin1 = 26; // ADC pin 1
-const int adcPin2 = 27; // ADC pin 2
-const int gearHigh = 2; // Digital input pin number
+const int adcPin1 = 26; 
+const int adcPin2 = 27; 
+const int gearHigh = 2; 
 
-// Create a joystick object
 Joystick_ Joystick;
 
-// Define the button ranges
 const int numButtons = 8;
 int buttonRanges[numButtons][2];
 
-// Define EEPROM addresses for storing button ranges and range offset
 const int EEPROMAddress = 0;
 const int EEPROMRangeOffsetAddress = EEPROMAddress + sizeof(buttonRanges);
 
-// Define default button ranges
 const int defaultButtonRanges[numButtons][2] = {
   {100, 100},   // Button 1
   {100, 200},   // Button 2       The logic works like this: if a input pins is high(hence the name, if a physical button is pressed), 
@@ -45,19 +41,15 @@ void setup() {
   Serial.begin(9600);
   analogReadResolution(12); // Set ADC resolution to 12 bits
 
-  // Initialize the joystick library
   Joystick.begin();
 
-  // Wait for serial connection
   while (!Serial) {
     delay(10);
   }
 
-  // Check if button ranges are stored in EEPROM
   bool loadFromEEPROM = false;
   if (EEPROM.read(EEPROMAddress) == 'R' && EEPROM.read(EEPROMAddress + 1) == 'A' &&
       EEPROM.read(EEPROMAddress + 2) == 'N' && EEPROM.read(EEPROMAddress + 3) == 'G') {
-    // Read button ranges from EEPROM
     for (int i = 0; i < numButtons; i++) {
       int eepromAddr = EEPROMAddress + 4 + i * 4;
       for (int j = 0; j < 2; j++) {
@@ -70,12 +62,10 @@ void setup() {
     loadFromEEPROM = true;
   }
 
-  // If button ranges were not loaded from EEPROM, use default ranges
   if (!loadFromEEPROM) {
     memcpy(buttonRanges, defaultButtonRanges, sizeof(buttonRanges));
   }
 
-  // Validate button ranges
   for (int i = 0; i < numButtons; i++) {
     if (buttonRanges[i][0] < 0 || buttonRanges[i][0] >= 4096 ||
         buttonRanges[i][1] < 0 || buttonRanges[i][1] >= 4096) {
@@ -85,7 +75,6 @@ void setup() {
     }
   }
 
-  // Store button ranges and range offset in EEPROM if not loaded from EEPROM
   if (!loadFromEEPROM) {
     EEPROM.write(EEPROMAddress, 'R');
     EEPROM.write(EEPROMAddress + 1, 'A');
@@ -103,7 +92,6 @@ void setup() {
     EEPROM.write(EEPROMRangeOffsetAddress, rangeOffset);
   }
 
-  // Print configured button ranges
   Serial.println("Configured Button Ranges:");
   for (int i = 0; i < numButtons; i++) {
     Serial.print("Button ");
@@ -114,7 +102,6 @@ void setup() {
     Serial.println(buttonRanges[i][1]);
   }
 
-  // If loaded from EEPROM, print the loaded button ranges and range offset
   if (loadFromEEPROM) {
     Serial.println("Loaded Button Ranges:");
     for (int i = 0; i < numButtons; i++) {
@@ -129,7 +116,6 @@ void setup() {
     Serial.println(rangeOffset);
   }
 
-  // Check if max button allowed or range offset is set via serial
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     if (input.startsWith("MAXBUTTON")) {
@@ -163,7 +149,6 @@ void loop() {
   Serial.print("\tADC 2: ");
   Serial.println(value2);
 
-  // Check button ranges
   for (int i = 0; i < maxButtonAllowed; i++) {
     int adc1Min = buttonRanges[i][0] - rangeOffset;
     int adc1Max = buttonRanges[i][0] + rangeOffset;
@@ -171,20 +156,17 @@ void loop() {
     int adc2Max = buttonRanges[i][1] + rangeOffset;
 
     if (value1 > adc1Min && value1 < adc1Max && value2 > adc2Min && value2 < adc2Max) {
-      // Simulate button press
       int buttonNum = i + 1;
       if (digitalRead(gearHigh) == HIGH) {
         buttonNum += 8;
       }
       Joystick.pressButton(buttonNum);
     } else {
-      // Release button
       Joystick.releaseButton(i + 1);
     }
   }
 
-  // Send joystick data
   Joystick.sendState();
 
-  delay(100); // Adjust delay as needed
+  delay(10);
 }
