87e1524426eab1d3a0220b447084883881afb1a3
OrhanYigitDurmaz <orhanyigitv2durmaz@gmail.com>
2023-07-31 17:03:24 +0300

    Added eeprom write functions

--- SimShiftElite/readSerial.cpp
+++ SimShiftElite/readSerial.cpp
@@ -29,15 +29,44 @@ void readSerial() {
             Serial.println(number);
 
         } else if (receivedData == "shifter_1") {
+            data[1].value1 = analogRead(A0);
+            data[1].value2 = analogRead(A1);
             Serial.println("S1 CALIBRATED");
 
-            //EEPROM.write()
-
         } else if (receivedData == "shifter_2") {
+            data[2].value1 = analogRead(A0);
+            data[2].value2 = analogRead(A1);
             Serial.println("S2 CALIBRATED");
 
         } else if (receivedData == "shifter_3") {
+            data[3].value1 = analogRead(A0);
+            data[3].value2 = analogRead(A1);
             Serial.println("S3 CALIBRATED");
+
+        } else if (receivedData == "shifter_4") {
+            data[4].value1 = analogRead(A0);
+            data[4].value2 = analogRead(A1);
+            Serial.println("S4 CALIBRATED");
+
+        } else if (receivedData == "shifter_5") {
+            data[5].value1 = analogRead(A0);
+            data[5].value2 = analogRead(A1);
+            Serial.println("S5 CALIBRATED");
+
+        } else if (receivedData == "shifter_6") {
+            data[6].value1 = analogRead(A0);
+            data[6].value2 = analogRead(A1);
+            Serial.println("S6 CALIBRATED");
+        
+        } else if (receivedData == "shifter_7") {
+            data[7].value1 = analogRead(A0);
+            data[7].value2 = analogRead(A1);
+            Serial.println("S7 CALIBRATED");
+
+        } else if (receivedData == "shifter_8") {
+            data[8].value1 = analogRead(A0);
+            data[8].value2 = analogRead(A1);
+            Serial.println("S8 CALIBRATED");
 
         } else {
             Serial.println("WRONG_FORMAT");
